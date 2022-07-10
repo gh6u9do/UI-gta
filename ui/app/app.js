8d298046d4ee5ea6be2e62076fb652df8ee13da3
gh6u9do <thedovakin31@mail.ru>
2022-07-10 22:37:35 +0300

    добавлен event manager

--- ui/app/app.js
+++ ui/app/app.js
@@ -5,10 +5,22 @@ import React from 'react'
 
 import StatusBar from "./components/StatusBar/StatusBar"
 import Avtorization from './layouts/avtorization/avtorization'
+import {EventManager} from './bridge'
 
 function App() {
+    const [showStatusBar, setStatusBar] = React.useState(true)
+    React.useEffect(() => {
+        EventManager.addHandler("toggleStatusBarView", toggleStatusBarView);
+        return () => {
+            EventManager.removeHandler("toggleStatusBarView", toggleStatusBarView);
+        }
+    }, [])
+    
+    function toggleStatusBarView (flag) {
+        setStatusBar(flag);
+    }
     return <div className='App'>
-        <StatusBar />
+        {showStatusBar && <StatusBar />}
         <Avtorization />
     </div>
 }
